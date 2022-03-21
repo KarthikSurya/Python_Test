220497692b7f7af319b2e433caf851e244bbf6fe
KarthikSurya <sampathirao.karthik@gmail.com>
2022-03-21 21:59:02 +0530

    New Python Test file

---
+++ main.py
@@ -0,0 +1,14 @@
+# this is a test python script
+
+print("hello World")
+
+a = "Python"
+b = "Poweful programmming language"
+
+print(a+" "+b)
+print("{} is {}".format(a, b))
+
+c = 3.1467
+print("Values of Pie is {}".format(c))
+
+
